41194895e29f68c927a8c9197b28632a1230b5e7
AnetaPopelova <aneta.popelova@gmail.com>
2023-07-26 18:32:23 +0200

    zmena zadani

--- uvod-do-programovani-1/6-podminky/excs/3-registrace.py
+++ uvod-do-programovani-1/6-podminky/excs/3-registrace.py
@@ -1,8 +1,8 @@
-# Založte si program registrace.py.
-# Program nechá uživatele, aby si zvolil uživatelské jméno a heslo.
-# Heslo jej nechejte zadat dvakrát a ověřte, že jej uživatel zadal dvakrát stejně.
-# V opačném případě vypište varování, že hesla nejsou stejná.
-# Při prvním zadávání ověřte, že heslo je bezpečné, což v tomto případě znamená, že je delší než 8 znaků.
+# Založte si program registrace.py. 
+# Program nechá uživatele, aby si zvolil uživatelské jméno a heslo. 
+# Heslo jej nechejte zadat dvakrát a ověřte, že jej uživatel zadal dvakrát stejně. 
+# V opačném případě vypište varování, že hesla nejsou stejná. 
+# Dále ověřte, že heslo je bezpečné, což v tomto případě znamená, že je delší než 8 znaků.
 
 uzivatel = input("Zadej uživatelské jméno: ")
 
