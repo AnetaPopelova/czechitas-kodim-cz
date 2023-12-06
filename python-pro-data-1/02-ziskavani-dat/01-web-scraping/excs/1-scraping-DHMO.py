ba01d035f320fdc7d25b1e3c50d4e367924da284
AnetaPopelova <aneta.popelova@gmail.com>
2023-12-06 19:47:56 +0100

    new file structure

--- python-pro-data-1/02-ziskavani-dat/01-web-scraping/excs/1-scraping-DHMO.py
+++ python-pro-data-1/02-ziskavani-dat/01-web-scraping/excs/1-scraping-DHMO.py
@@ -7,14 +7,14 @@ session = HTMLSession()
 stranka = session.get("https://apps.kodim.cz/python-data/dhmo")
 
 # Nechť program vypíše na výstup nadpisy všech sekcí (značka h2).
-for nadpis in stranka.html.find("h2"):
-    print(nadpis.text)
+# for nadpis in stranka.html.find("h2"):
+#     print(nadpis.text)
 
 # Nechť program vypíše na výstup cesty všech odkazů na stránce (značka a, atribut href).
-for odkaz in stranka.html.find("a"):
-    print(odkaz.attrs["href"])
+# for odkaz in stranka.html.find("a"):
+#     print(odkaz.attrs["href"])
 
-# Nechť program vypíše na výstup cesty ke všem obrázkům na stránce (značka img, atribut src).
+# # Nechť program vypíše na výstup cesty ke všem obrázkům na stránce (značka img, atribut src).
 for obrazek in stranka.html.find("img"):
     print(obrazek.attrs["src"])
 
